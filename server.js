b87f07b71c28b69685fe69a230bc41729b7be32a
JEStudyhall <jaieelizabeth@gmail.com>
2026-01-14 15:29:10 -0500

    filter query

--- server.js
+++ server.js
@@ -28,10 +28,38 @@ app.get("/collectibles/:index"),
       res.send("This item is not yet in stock. Check back soon!");
     }
   };
-git ad
+
 //=================================================
 
-app.get("shoes", (req, res) => {});
+app.get("/shoes", (req, res) => {
+  const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" },
+  ];
+  const minPrice = +req.query["min-price"];
+  const maxPrice = +req.query["max-price"];
+  const shoeType = req.query["type"];
+  let filteredShoes = [...shoes];
+  if (minPrice) {
+    filteredShoes = filteredShoes.filter((shoe) => shoe.price >= minPrice);
+  }
+  console.log(filteredShoes);
+  if (maxPrice) {
+    filteredShoes = filteredShoes.filter((shoe) => shoe.price <= maxPrice);
+  }
+  console.log(filteredShoes);
+  if (shoeType) {
+    filteredShoes = filteredShoes.filter((shoe) => shoe.type === shoeType);
+  }
+  console.log(filteredShoes);
+  res.send(filteredShoes);
+});
+
 
 app.listen(3000, () => {
   console.clear();
